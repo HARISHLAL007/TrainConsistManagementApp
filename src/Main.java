772b3461f4208659152dc23bfa0d73ae6fb34866
HARISHLAL <harishlalme9a@gmail.com>
2026-04-10 14:57:16 +0530

    UC18: Linear search for bogie ID

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,27 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Train Consist Management App Started");
+
+        System.out.println("=== UC18: Linear Search for Bogie ID ===");
+
+        String[] bogieIds = {"BG101", "BG205", "BG309", "BG412", "BG550"};
+
+        String searchKey = "BG309"; // change value to test
+
+        boolean found = false;
+
+        for (String id : bogieIds) {
+            if (id.equals(searchKey)) {
+                found = true;
+                break;
+            }
+        }
+
+        if (found) {
+            System.out.println("Bogie ID " + searchKey + " FOUND");
+        } else {
+            System.out.println("Bogie ID " + searchKey + " NOT FOUND");
+        }
+
+        System.out.println("Program continues...");
     }
 }
