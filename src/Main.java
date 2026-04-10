30882ca83168ac89a49f3224eb32dad93a09177c
HARISHLAL <harishlalme9a@gmail.com>
2026-04-10 14:54:34 +0530

    UC16: Sort passenger capacities using Bubble Sort

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,32 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Train Consist Management App Started");
+
+        System.out.println("=== UC16: Bubble Sort Passenger Capacities ===");
+
+        int[] capacities = {72, 56, 24, 70, 60};
+
+        System.out.println("Before Sorting:");
+        for (int c : capacities) {
+            System.out.print(c + " ");
+        }
+
+        // Bubble Sort
+        int n = capacities.length;
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = 0; j < n - i - 1; j++) {
+                if (capacities[j] > capacities[j + 1]) {
+                    int temp = capacities[j];
+                    capacities[j] = capacities[j + 1];
+                    capacities[j + 1] = temp;
+                }
+            }
+        }
+
+        System.out.println("\nAfter Sorting:");
+        for (int c : capacities) {
+            System.out.print(c + " ");
+        }
+
+        System.out.println("\nProgram continues...");
     }
 }
