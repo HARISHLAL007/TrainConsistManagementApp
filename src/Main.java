59aa8914a7c543371fdb994570d4eb95c61f20a2
HARISHLAL <harishlalme9a@gmail.com>
2026-04-10 14:55:48 +0530

    UC17: Sort bogie names using Arrays.sort()

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,27 @@
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Train Consist Management App Started");
+
+        System.out.println("=== UC17: Sort Bogie Names ===");
+
+        String[] bogieNames = {
+                "Sleeper",
+                "AC Chair",
+                "First Class",
+                "General",
+                "Luxury"
+        };
+
+        System.out.println("Before Sorting:");
+        System.out.println(Arrays.toString(bogieNames));
+
+        // Using built-in sort
+        Arrays.sort(bogieNames);
+
+        System.out.println("After Sorting:");
+        System.out.println(Arrays.toString(bogieNames));
+
+        System.out.println("Program continues...");
     }
 }
