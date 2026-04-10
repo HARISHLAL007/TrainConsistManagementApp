b56f21c786d7d80e4426d12456fbc08a7d0cfbef
HARISHLAL <harishlalme9a@gmail.com>
2026-04-10 14:37:53 +0530

    UC6: Map bogie to capacity using HashMap

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Train Consist Management App Started");
-    }
-}
+// ---------------- UC6 ----------------
+System.out.println("-----------------------------------");
+
+HashMap<String, Integer> bogieCapacity = new HashMap<>();
+
+// Adding bogie capacities
+bogieCapacity.put("Sleeper", 72);
+bogieCapacity.put("AC Chair", 60);
+bogieCapacity.put("First Class", 24);
+bogieCapacity.put("Cargo", 100);
+
+// Displaying capacity details
+System.out.println("Bogie Capacity Details:");
+
+for (Map.Entry<String, Integer> entry : bogieCapacity.entrySet()) {
+        System.out.println(entry.getKey() + " -> " + entry.getValue());
+        }
