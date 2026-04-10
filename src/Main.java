46841cc02a7a9bd60ac1823baaab129cf066d091
HARISHLAL <harishlalme9a@gmail.com>
2026-04-10 14:29:29 +0530

    UC3: Track unique bogie IDs using HashSet

--- src/Main.java
+++ src/Main.java
@@ -6,7 +6,6 @@ public class Main {
 
         System.out.println("=== Train Consist Management App ===");
 
-        // UC3: Track Unique Bogie IDs using HashSet
         Set<String> bogieIds = new HashSet<>();
 
         System.out.println("Adding bogie IDs...");
@@ -14,8 +13,8 @@ public class Main {
         bogieIds.add("BG101");
         bogieIds.add("BG102");
         bogieIds.add("BG103");
-        bogieIds.add("BG101"); // duplicate
-        bogieIds.add("BG102"); // duplicate
+        bogieIds.add("BG101");
+        bogieIds.add("BG102");
 
         System.out.println("Final unique bogie IDs:");
         System.out.println(bogieIds);
