e676eaa39ed75f3243fd609b25567a672b2d8ba4
HARISHLAL <harishlalme9a@gmail.com>
2026-04-10 14:40:41 +0530

    UC7: Sort bogies using Comparator

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,43 @@
+import java.util.*;
+
+class Bogie {
+    String name;
+    int capacity;
+
+    Bogie(String name, int capacity) {
+        this.name = name;
+        this.capacity = capacity;
+    }
+
+    @Override
+    public String toString() {
+        return name + " -> " + capacity;
+    }
+}
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Train Consist Management App Started");
+
+        System.out.println("=== UC7: Sort Bogies by Capacity ===");
+
+        List<Bogie> bogies = new ArrayList<>();
+
+        bogies.add(new Bogie("Sleeper", 72));
+        bogies.add(new Bogie("AC Chair", 60));
+        bogies.add(new Bogie("First Class", 24));
+
+        System.out.println("Before Sorting:");
+        for (Bogie b : bogies) {
+            System.out.println(b);
+        }
+
+        bogies.sort(Comparator.comparingInt(b -> b.capacity));
+
+        System.out.println("----------------------------");
+        System.out.println("After Sorting (by capacity):");
+
+        for (Bogie b : bogies) {
+            System.out.println(b);
+        }
     }
 }
